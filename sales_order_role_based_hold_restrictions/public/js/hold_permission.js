232a8d65938563decf71b6e9e5e480371ae629f7
aniket <anikets@criscoconsulting.in>
2020-12-09 12:27:06 +0000

    console log

--- sales_order_role_based_hold_restrictions/public/js/hold_permission.js
+++ sales_order_role_based_hold_restrictions/public/js/hold_permission.js
@@ -5,6 +5,7 @@ frappe.ui.form.on("Sales Order",{
             method:"sales_order_role_based_hold_restrictions.sales_order_role_based_hold_restrictions.doctype.hold_permissions.hold_permissions.get_roles",
             args:{"docname":frm.doctype},
             callback:function(r){
+                console.log(r.message)
                 if(r.message){
                     if((flt(doc.per_delivered, 6) < 100 || flt(doc.per_billed) < 100) && has_common(frappe.user_roles, r.message)) {
 							// hold
@@ -26,6 +27,7 @@ frappe.ui.form.on("Sales Invoice",{
             args:{"docname":frm.doctype},
             callback:function(r){
                 if(r.message){
+                    console.log(r.message)
                     if((flt(doc.per_delivered, 6) < 100 || flt(doc.per_billed) < 100) && has_common(frappe.user_roles, r.message)) {
 							// hold
 							this.frm.remove_custom_button(__('Hold'), __("Status"))
@@ -46,6 +48,7 @@ frappe.ui.form.on("Purchase Order",{
             args:{"docname":frm.doctype},
             callback:function(r){
                 if(r.message){
+                    console.log(r.message)
                     if((flt(doc.per_delivered, 6) < 100 || flt(doc.per_billed) < 100) && has_common(frappe.user_roles, r.message)) {
 							// hold
 							this.frm.remove_custom_button(__('Hold'), __("Status"))
